12d728fba8faffa0b5db79b2463e583fba582776
hmdali98 <73855016+hmdali98@users.noreply.github.com>
2022-03-05 22:53:15 +0400

    initial commit

---
+++ lib/widgets/CategoryItem.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:missedlecture/Views/FoodPage.dart';
+import 'package:missedlecture/models/category.dart';
+
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+  const CategoryItem({this.category,key}) : super(key: key);
+
+  //TODO: git hub
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return FoodPage(category: category,);
+        },));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: category.color,
+              borderRadius: BorderRadius.circular(10)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(category.title, style: Theme.of(context).textTheme.headline6,),
+          ),
+        ),
+      ),
+    );
+  }
+}
